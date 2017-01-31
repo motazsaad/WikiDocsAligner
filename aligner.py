7bbab2d4fa61956c289e3a114d17f0c3d2179c67
Motaz Saad <motaz.saad@gmail.com>
2017-01-31 19:17:00 +0200

    load wiki extracts

--- aligner.py
+++ aligner.py
@@ -5,23 +5,23 @@ import read_wiki_extracts
 import worker
 
 
-def main(src_lang, target_lang, src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir):
+def main(src_lang, target_lang, src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir, out_dir):
     print('source language:', src_lang)
     print('target language:', target_lang)
     src_df = parse_sql_script.sql2df(src_ll_sql)
     target_df = parse_sql_script.sql2df(target_ll_sql)
     src_corpus = read_wiki_extracts.load_corpus(src_corpus_dir)
     target_corpus = read_wiki_extracts.load_corpus(target_corpus_dir)
-    worker.doJob(src_lang, target_lang, src_df, target_df, src_corpus, target_corpus)
+    worker.doJob(src_lang, target_lang, src_df, target_df, src_corpus, target_corpus, out_dir)
 
 
 
 
 def usage():
-    return "args should be: src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir"
+    return "args should be: src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir, out_dir"
 
 if __name__ == '__main__':
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 8:
         print(usage())
         sys.exit(0)
     else:
@@ -31,4 +31,5 @@ if __name__ == '__main__':
         target_ll_sql = sys.argv[4]
         src_corpus_dir = sys.argv[5]
         target_corpus_dir = sys.argv[6]
-        main(src_lang, target_lang, src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir)
+        out_dir = sys.argv[7]
+        main(src_lang, target_lang, src_ll_sql, target_ll_sql, src_corpus_dir, target_corpus_dir, out_dir)
--- worker.py
+++ worker.py
@@ -1,2 +1,31 @@
-def doJob(src_lang, target_lang, src_df, target_df, src_corpus, target_corpus):
-    pass
+import os
+
+def getTargetTitle(df, doc_id, ll_lang):
+    return df.loc[(df.ll_from == doc_id) & (df.ll_lang == ll_lang), 'll_title'].values[0]
+
+
+def getDocByTitle(title, corpus):
+    for wiki_doc in corpus:
+        doc_id, doc_title, doc = wiki_doc
+        if doc_title == title:
+            return wiki_doc
+    return None
+
+def save_docs(aligned_docs, out_dir, src_lang, target_lang):
+    src_path = os.path.join(out_dir, src_lang)
+    target_path = os.path.join(out_dir, target_lang)
+    if not os.path.exists(src_path): os.makedirs(src_path)
+    if not os.path.exists(target_path): os.makedirs(target_path)
+    for s,t in aligned_docs:
+
+
+
+def doJob(src_lang, target_lang, src_df, target_df, src_corpus, target_corpus, out_dir):
+    aligned_docs = list()
+    for src_doc in src_corpus:
+        doc_id, title, doc = src_doc
+        target_title = getTargetTitle(src_df, doc_id, target_lang)
+        target_doc = getDocByTitle(target_title, target_corpus)
+        aligned_docs.append((src_doc, target_doc))
+
+    save_docs(aligned_docs, out_dir, src_lang, target_lang)
