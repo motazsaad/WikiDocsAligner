6e035734967f91192c1d3d7419e854595636a518
Motaz Saad <motaz.saad@gmail.com>
2017-01-31 18:22:02 +0200

    load wiki extracts

--- tmp4.py
+++ tmp4.py
@@ -3,13 +3,12 @@ import os
 import parse_sql_script
 
 
-def ge_title(df, doc_id, ll_lang):
-    return df.loc[df['ll_from'] == doc_id, 'll_lang'].values[0]
-
+def getTargetTitle(df, doc_id, ll_lang):
+    return df.loc[(df.ll_from == doc_id) & (df.ll_lang == ll_lang), 'll_title'].values[0]
 
 
 arz_df, ar_df = parse_sql_script.load_df()
 
 
 
-print(ge_title(ar_df, 7, 'arz'))
+print(getTargetTitle(ar_df, '7', 'arz'))
