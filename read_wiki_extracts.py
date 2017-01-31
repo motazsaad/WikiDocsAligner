ee53b7ea2862e30363e8326bf353444dafe01a1c
Motaz Saad <motaz.saad@gmail.com>
2017-01-31 14:12:12 +0200

    load wiki extracts

--- read_wiki_extracts.py
+++ read_wiki_extracts.py
@@ -7,18 +7,18 @@ import os
 
 
 def load_corpus(corpus_dir):
-    wiki_docs = list()
+    corpus = list()
     for subdir, dirs, files in os.walk(corpus_dir):
         for f in files:
             wiki_file = os.path.join(subdir, f)
             with open(wiki_file, encoding='utf-8') as wiki_reader:
                 text = wiki_reader.read()
-                wiki_docs.append(text)
+                corpus.append(text)
 
 
 
-def align_docs(wiki_doc):
-        soup = BeautifulSoup(wiki_doc, 'html.parser')
+def align_docs(corpus):
+        soup = BeautifulSoup(corpus, 'html.parser')
         docs = soup.find_all('doc')
         for doc in docs:
             id = doc.get('id')
