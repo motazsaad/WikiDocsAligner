6cbb98845b53ce884a08770e34511492740fb183
Motaz Saad <motaz.saad@gmail.com>
2017-04-30 07:10:41 +0300

    update args

--- aligner.py
+++ aligner.py
@@ -28,40 +28,33 @@ def main(src_lang, target_lang, src_ll_sql, src_corpus_dir, target_corpus_dir, o
     worker.do_work(src_lang, target_lang, src_df, src_corpus, target_corpus, out_dir)
 
 
-def usage():
-    return '''args should be:
-    source language,
-    target language,
-    source language links sql file,
-    source corpus directory,
-    target corpus directory,
-    output directory'''
+
 
 
 parser = argparse.ArgumentParser(description='Align Wikipedia documents based on interlanguage links .')
 
-parser.add_argument('--srcLang', type=str, help='source language. '
+parser.add_argument('--src-lang', type=str, help='source language. '
                                                 'e.g., ar for Arabic, '
                                                 'en for English, or '
                                                 'fr for French ...', required=True)
-parser.add_argument('--targetLang', type=str, help='target language. '
+parser.add_argument('--target-lang', type=str, help='target language. '
                                                    'e.g., ar for Arabic, '
                                                    'en for English, or '
                                                    'fr for French ...', required=True)
-parser.add_argument('--sqlFile', type=str, help='source language links sql file. '
+parser.add_argument('--sql-file', type=str, help='source language links sql file. '
                                                 'Obtained from https://dumps.wikimedia.org/', required=True)
-parser.add_argument('--srcCorpus', type=str, help='source corpus directory.', required=True)
-parser.add_argument('--targetCorpus', type=str, help='target corpus directory.', required=True)
-parser.add_argument('--outDir', type=str, help='the output directory.', required=True)
+parser.add_argument('--src-corpus', type=str, help='source corpus directory.', required=True)
+parser.add_argument('--target-corpus', type=str, help='target corpus directory.', required=True)
+parser.add_argument('--out-dir', type=str, help='the output directory.', required=True)
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    src_lang = args.srcLang
-    target_lang = args.targetLang
-    src_ll_sql = args.sqlFile
-    src_corpus_dir = args.srcCorpus
-    target_corpus_dir = args.targetCorpus
-    out_dir = args.outDir
+    src_lang = args.src_lang
+    target_lang = args.target_lang
+    src_ll_sql = args.sql_file
+    src_corpus_dir = args.src_corpus
+    target_corpus_dir = args.target_corpus
+    out_dir = args.out_dir
     main(src_lang, target_lang, src_ll_sql, src_corpus_dir, target_corpus_dir, out_dir)
 
 '''
